46ee70e50c4225c62614efc77851ddc1efaa883a
Rakeshatla <151203741+Rakeshatla@users.noreply.github.com>
2024-11-06 23:01:17 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const alienRouter = require('./routes/aliens')
+
+
+const url = 'mongodb://127.0.0.1:27017/CBIT';
+const app = express()
+mongoose.connect(url)
+const con = mongoose.connection
+
+
+con.on('open', () => {
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+app.use('/', alienRouter)
+app.listen(9000, () => {
+    console.log('Server started')
+})
